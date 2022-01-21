29ce4712d710c726aab25e9311188c55d3928055
Vladimir <penjavladimir@gmail.com>
2022-01-21 17:31:22 +0200

    les2

---
+++ Lesson2/task1.js
@@ -0,0 +1,12 @@
+/* Перепишите код, заменив оператор `if` на тернарный оператор `?`
+var result;
+if (a + b < 4) {
+result = true;
+} else {
+result = false;
+}
+*/
+let a = 1;
+let b = 2;
+result = (a + b < 4) ? result = true : result = false;
+console.log(result);
