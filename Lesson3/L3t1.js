c2a3df79a7e4ef71fe09580f0ac761f122d4d2fd
Vladimir <penjavladimir@gmail.com>
2022-01-21 18:17:01 +0200

    L3t1

---
+++ Lesson3/L3t1.js
@@ -0,0 +1,18 @@
+/*
+####Задача 1
+Напишите функцию `f`, которая возвращает куб числа. Число передается параметром. 
+Данная функция должна обязательно содержать проверку входного параметра, потому что принимать она может только число.
+
+```js
+f(2); // 8
+f('Content'); // Error: parameter type is not a Number
+```
+*/
+
+function f (n){
+    if (typeof n === 'number') {
+        console.log(Math.pow(n,3));
+    } else throw new Error("parameter type is not a Number");
+};
+f(2);
+f('2');
