362df8ac13d141a284312a5514deda4c53b01f45
Vladimir <penjavladimir@gmail.com>
2022-01-21 18:21:02 +0200

    L3t1

--- Lesson3/L3t1.js
+++ Lesson3/L3t1.js
@@ -8,11 +8,12 @@ f(2); // 8
 f('Content'); // Error: parameter type is not a Number
 ```
 */
-
 function f (n){
     if (typeof n === 'number') {
         console.log(Math.pow(n,3));
-    } else throw new Error("parameter type is not a Number");
+    } else {
+        throw new Error("parameter type is not a Number");
+    }
 };
 f(2);
 f('2');
