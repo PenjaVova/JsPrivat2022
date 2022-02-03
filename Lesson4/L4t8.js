7c659ba0bd8d6b3bb34aaa853148921ae56550f4
Vladimir <penjavladimir@gmail.com>
2022-02-03 22:33:15 +0200

    bugFix

--- Lesson4/L4t8.js
+++ Lesson4/L4t8.js
@@ -1,6 +1,5 @@
 /*
-Назначено
-####Задача 8
+Задача 8
 
 Напишите функцию `f` . Данная функция принимает один параметр: одноуровневый или многоуровневый массив. Возвращает данная функция сумму всех элементов на всех уровнях.
 
@@ -10,16 +9,43 @@
 
 - Первый параметр обязателен и может принимать только массив
 - Генерировать ошибку если на каком то уровне было найдено не число и не массив
+ */
+function f(arr) {
 
-```javascript
+    let sum = 0;
+
+    function inner (arr) {
+        if(Array.isArray(arr)){
+            arr.forEach(elem =>inner(elem))
+        } else {
+            if(typeof arr === 'number') {
+
+                return sum+=arr;
+
+            } else {
+           
+                throw new Error("wrong argument type");
+
+            }
+        }
+
+        return sum;
+    }
+    return inner(arr);
+}
+
 const arr = [[[1, 2], [1, 2]], [[2, 1], [1, 2]]];
-f(arr); // 12
+
 const arr2 = [[[[[1,2]]]]];
-f(arr2); // 3
+
 const arr3 = [[[[[1,2]]],2],1];
-f(arr3); // 6
+
 const arr4 = [[[[[]]]]];
-f(arr4); // 0
+
 const arr5 = [[[[[],3]]]];
-f(arr5); // 3
-*/
+
+ console.log(f(arr)); // 12
+ console.log(f(arr2)); // 3
+ console.log(f(arr3)); // 6
+ console.log(f(arr4)); // 0
+ console.log(f(arr5)); // 3
