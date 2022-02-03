8e1fd9213435d073b1fedd44124631949eb04682
Vladimir <penjavladimir@gmail.com>
2022-02-03 23:26:06 +0200

    bugFix

--- Lesson4/L4t10.js
+++ Lesson4/L4t10.js
@@ -12,3 +12,28 @@
 const arr = [3,2,1];
 reverse(arr); // [1,2,3]
 */
+
+function reverse (arr) { 
+
+    if(!Array.isArray(arr)||arr.length==0){
+        throw new Error ("array must have elements")
+    }
+
+    let maxI = Math.floor(arr.length/2)
+
+    for (let i = 0; i < arr.length; i++) {
+        
+        if (i==maxI) break;
+
+            let tmp = arr[i];
+            arr[i]= arr[arr.length-i-1];
+            arr[arr.length-i-1] = tmp;
+
+       
+    }
+    console.log(arr.toString());
+}
+
+
+const arr = [3,2,1];
+reverse(arr);
